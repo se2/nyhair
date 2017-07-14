4d7a3dc73a51078d8a85935b0899d65fd4f5757c
Tu Luong <luongduc.tu1992@gmail.com>
2017-07-14 18:15:32 -0400

    Apply video background homepage

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php get_header(); ?>
 
-<div class="home home--bg home--fullpage" style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/images/front-page.png');">
+<video class="video-bg video-bg__desktop" poster="<?php echo get_stylesheet_directory_uri(); ?>/images/poster.png" playsinline autoplay muted loop>
+	<source src="<?php echo get_stylesheet_directory_uri(); ?>/images/nyh-homepage-video.mp4" type="video/mp4">
+</video>
+
+<div class="video-bg video-bg__mobile" style="background-image: url('<?php echo get_stylesheet_directory_uri(); ?>/images/front-page__mobile.png');"></div>
+
+<div class="home home--bg home--fullpage">
 	<img src="<?php echo IMG_PATH; ?>/home-logo.png" alt="NY Hair" class="home--logo">
 </div>
 
